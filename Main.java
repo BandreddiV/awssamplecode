ca1e1d73c87e005db06077094df104e963225069
Venkata Narayana Murthy Bandreddi <Venkata Narayana Murthy Bandreddi>
2017-06-11 11:49:09 -0500

    Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = '\u00A9';
+        System.out.println("Unicode Output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char myReg = '\u00AE';
+        System.out.println("For Register is = " + myReg);
+    }
+}
